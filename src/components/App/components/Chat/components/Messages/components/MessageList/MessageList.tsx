3411ab5b5b172589278c14d4ba1083c2dd160f9e
Daniels Holden <danielsholden.my@gmail.com>
2023-12-04 01:30:19 +0400

    added scroll

--- src/components/App/components/Chat/components/Messages/components/MessageList/MessageList.tsx
+++ src/components/App/components/Chat/components/Messages/components/MessageList/MessageList.tsx
@@ -9,10 +9,20 @@ import styles from './MessageList.module.scss';
 
 const MessageList: React.FC = () => {
   const messages = useSelector(selectMessages);
+  const containerRef = React.useRef(null)
+
+  const scrollToBottom = (): void =>
+    containerRef.current?.scrollIntoView({ behavior: 'smooth' });
+
+  React.useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
     <div className={styles.messages}>
       {messages.map((message, idx) => <Message key={idx} text={message} />)}
       {!messages.length && <p className={styles.noMessages}>No messages yet</p>}
+      <div ref={containerRef} />
     </div>
   )
 };
